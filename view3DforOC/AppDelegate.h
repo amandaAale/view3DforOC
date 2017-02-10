8ff631adfb02b1511c5f70ea18893bac5402891c
陈森宇 <senyu@senyu-macbook-air.local>
2017-02-10 10:47:15 +0800

    Initial Commit

---
+++ view3DforOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  view3DforOC
+//
+//  Created by senyu on 2017/2/10.
+//  Copyright © 2017年 senyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ view3DforOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  view3DforOC
+//
+//  Created by senyu on 2017/2/10.
+//  Copyright © 2017年 senyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
